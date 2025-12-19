48a9859a904328822ab051cd716afb3975b1b4eb
www391pr <www391787@gmail.com>
2025-12-19 15:51:05 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 return [
     'paths' => ['api/*'],   // apply CORS to API routes
     'allowed_methods' => ['*'],  // GET, POST, PUT, DELETE, OPTIONS
-    'allowed_origins' => ['https://your-frontend.onrender.com'], // your frontend URL
+    'allowed_origins' => ['https://lms-frontend-react.onrender.com'], // your frontend URL
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
